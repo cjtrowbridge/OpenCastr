8bd6b485e7a5a8c631bfdbe3d6aa28eb3aedca68
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-26 21:44:41 -0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+  User Behaviors;
+    Users can sign up or log in via OAuth.
+    Users can browse podcasts and subscribe or unsubscribe.
+    Users have a playlist of episodes to listen to.
+    Progress is synchronized between user sessions.
+*/
+
 if(isset($_GET['playlist'])){
   header("Content-Type: application/json; charset=UTF-8");
   $File = file_get_contents('playlist.json');
