2d05af42acd0f80810137dd330bcd88a8749915d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-26 21:49:25 -0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -8,11 +8,6 @@
     Progress is synchronized between user sessions.
 */
 
-if(isset($_GET['playlist'])){
-  header("Content-Type: application/json; charset=UTF-8");
-  $File = file_get_contents('playlist.json');
-  die($File);
-}
-
-
-?>Not Found
+header("Content-Type: application/json; charset=UTF-8");
+$File = file_get_contents('userObject.json');
+die($File);
