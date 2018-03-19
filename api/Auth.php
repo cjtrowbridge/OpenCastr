3c3bb033bf5d8b1451922a478fc82f7c6fa5f619
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-03-19 15:09:35 -0700

    Update Auth.php

--- api/Auth.php
+++ api/Auth.php
@@ -10,4 +10,28 @@ function Auth(){
     echo PHP_EOL.'POST: '.PHP_EOL;
     var_dump($_POST);
   }
+
+  
+  require_once 'google-api-php-client/src/Google_Client.php';
+  require_once 'google-api-php-client/src/contrib/Google_Oauth2Service.php';
+  session_start();
+
+  global $GOOGLE;
+  $client = new Google_Client();
+  $client->setClientId($GOOGLE['ClientID']);
+  $client->setClientSecret($GOOGLE['ClientSecret']);
+
+  $oauth2 = new Google_Oauth2Service($client);
+
+  if (isset($_SESSION['token'])) {
+   $client->setAccessToken($_SESSION['token']);
+  }
+
+  if (isset($_REQUEST['logout'])) {
+    unset($_SESSION['token']);
+    $client->revokeToken();
+  }
+
+  
+  
 }
