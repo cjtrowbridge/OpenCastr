a7ca7a68f95194686dee798d6872ca49a06726a3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-04-12 10:26:48 -0700

    Update PublicPlaylist.php

--- api/PublicPlaylist.php
+++ api/PublicPlaylist.php
@@ -4,21 +4,21 @@ if(filemtime('PublicPlaylist.json') < time()-60*5){
   $Output = array(
     'Categories' => array(
       'News'        => array(
-        'NPR Hourly World News' => './api/?feed=npr-hourly-world-news',
-        'CBC Hourly World News' => './api/?feed=cbc-hourly-world-news',
-        'DW Hourly World News'  => './api/?feed=dw-hourly-world-news'
+        'NPR Hourly World News'  => './api/?feed=npr-hourly-world-news',
+        'CBC Hourly World News'  => './api/?feed=cbc-hourly-world-news',
+        'DW Hourly World News'   => './api/?feed=dw-hourly-world-news'
       ),
       'Inspiration' => array(
-        'Tim Ferriss'           => './api/?feed=tim-ferriss',
-        'Kevin Rose'            => './api/?feed=kevin-rose',
-        'Reid Hoffman'          => './api/?feed=reid-hoffman'
+        'Tim Ferriss'            => './api/?feed=tim-ferriss',
+        'Kevin Rose'             => './api/?feed=kevin-rose',
+        'Reid Hoffman'           => './api/?feed=reid-hoffman'
       ),
       'Learning'    => array(
-        'Economist'             => './api/?feed=economist',
-        'NPR: Planet Money'     => './api/?feed=planet-money',
-        'Stuff You Should Know' => './api/?feed=stuff-you-should-know',
-        '9% Invisible'          => './api/?feed=99-invisible',
-        'Star Talk'             => './api/?feed=star-talk'
+        'Economist: Money Talks' => './api/?feed=economist-money-talks',
+        'NPR: Planet Money'      => './api/?feed=planet-money',
+        'Stuff You Should Know'  => './api/?feed=stuff-you-should-know',
+        '9% Invisible'           => './api/?feed=99-invisible',
+        'Star Talk'              => './api/?feed=star-talk'
       )
     )
   );
