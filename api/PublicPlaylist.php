ebb13897186e13b8e98d4f4f46cf1db65ed8e385
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-04-12 10:16:13 -0700

    Update PublicPlaylist.php

--- api/PublicPlaylist.php
+++ api/PublicPlaylist.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if(filemtime('PublicPlaylist.json') < time()-60*5){
   $Output = array(
     'Categories' => array(
