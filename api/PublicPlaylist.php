644f645388581c2fafc01a182595037c2ffd6dd8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-04-27 15:38:39 -0700

    Update PublicPlaylist.php

--- api/PublicPlaylist.php
+++ api/PublicPlaylist.php
@@ -6,51 +6,62 @@ if(filemtime('PublicPlaylist.json') < time()-60*5){
       'News' => array(
         'NPR Hourly World News' => array(
           'Image' => 'http://toppodcast.com/wp-content/uploads/2017/05/5669-600x600bb.jpg',
-          'Feed'  => './api/?feed=npr-hourly-world-news'
+          'Feed'  => './api/?feed=npr-hourly-world-news',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'CBC Hourly World News' => array(
           'Image' => 'http://www.cbc.ca/podcasting/images/promo-hourlies.jpg',
           'Feed'  => './api/?feed=cbc-hourly-world-news',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'DW Hourly World News' => array(
           'Image' => 'http://www.dw.com/image/2135752_7.jpg',
-          'Feed'  => './api/?feed=dw-hourly-world-news'
+          'Feed'  => './api/?feed=dw-hourly-world-news',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         )
       ),
       'Inspiration' => array(
         'Tim Ferriss' => array(
           'Image' => 'https://secureimg.stitcher.com/feedimagesplain328/48065.jpg',
-          'Feed'  => './api/?feed=tim-ferriss'
+          'Feed'  => './api/?feed=tim-ferriss',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'Kevin Rose' => array(
           'Image' => '',
-          'Feed'  => './api/?feed=kevin-rose'
+          'Feed'  => './api/?feed=kevin-rose',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'Reid Hoffman' => array(
           'Image' => '',
-          'Feed'  => './api/?feed=reid-hoffman'
+          'Feed'  => './api/?feed=reid-hoffman',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         )
       ),
       'Learning' => array(
         'Economist: Money Talks' => array(
           'Image' => '',
-          'Feed'  => './api/?feed=economist-money-talks'
+          'Feed'  => './api/?feed=economist-money-talks',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'NPR: Planet Money' => array(
           'Image' => '',
           'Feed'  => './api/?feed=planet-money',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'Stuff You Should Know' => array(
           'Image' => '',
-          'Feed'  => './api/?feed=stuff-you-should-know'
+          'Feed'  => './api/?feed=stuff-you-should-know',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         '9% Invisible' => array(
           'Image' => '',
-          'Feed'  => './api/?feed=99-invisible'
+          'Feed'  => './api/?feed=99-invisible',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ),
         'Star Talk' => array(
           'Image' => '',
-          'Feed'  => './api/?feed=star-talk'
+          'Feed'  => './api/?feed=star-talk',
+          'Summary' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         )
       )
     )
